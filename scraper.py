a8edd149fabd2dd5c594618af381861d550e60c8
clayton <tortides@gmail.com>
2018-05-22 14:29:33 -0500

    removed test files and figured out the info is in json files. working on requesting the json files and grabbing the data from them

--- scraper.py
+++ scraper.py
@@ -1,15 +1,23 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from requests import get
+import json
 
-url = 'https://www.siriusxm.com/40sJunction'
+#define URL that will be requested
+url = 'https://www.siriusxm.com/40sJunction' #url you want to grab
 
-page = urlopen(url)
+#define the headers for the reqest of the JSON file
+#taken from network inspecting the webpage
+headers = {
+'User-agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+'X-Requested-With': 'XMLHttpRequest',
+'Referer': 'https://www.siriusxm.com/40sJunction',
+'Host' : 'www.siriusxm.com',
+'Accept-Encoding' : 'gzip, deflate, br',
+'Accept' : 'application/json, text/javascript, */*; q=0.01',
+'Accept-Language' : 'en-US,en;q=0.5',
+'Connection' : 'keep-alive'
+}
 
-parsed_page = BeautifulSoup(page, "html.parser")
+response = get(url) #request page and grab HTML
 
-#find artist
-#artist_box = parsed_page.find('div', attrs={'id' : 'onair-pdt'})
-artist_box = parsed_page.find('p', attrs={'id' : 'onair-pdt'})
-artist = artist_box.text.strip()
-print(artist_box)
+json = response.json()
 
