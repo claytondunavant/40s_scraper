53160ba199acdd4f2ee8f5050af3fa61544033c9
claytondunavant <tortides@gmail.com>
2018-05-23 15:39:52 -0500

    got it to check every minute. working on continuing after connection errors

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
-import requests, json, datetime
+import requests, json, datetime, openpyxl, time 
 
+#get_current_song()
 def get_current_song(): #gets song artist, album and name
     json_time = datetime.datetime.now(datetime.timezone.utc).strftime('%m-%d-%H:%M:00') #website uses UTC in this format for the json files that are updated at the top of every minute
     print(json_time)
@@ -14,6 +15,15 @@ def get_current_song(): #gets song artist, album and name
     album = json_dicts['song']['album']['name'] #grab album
     song = json_dicts['song']['name'] #grabe song
 
-    return artist, album, song
-   
-print(get_current_song())
+    return artist, album, song 
+
+print(requests.get("https://www.siriusxm.com/metadata/pdt/en-us/json/channels/8205/timestamp/05-23-15:50:00").json())
+
+'''
+exit = False
+
+while exit == False: #while exit is not true
+    if datetime.datetime.now(datetime.timezone.utc).strftime('%S') == '00': #if it is the top of the minute
+        print(get_current_song())
+        time.sleep(59)
+'''
